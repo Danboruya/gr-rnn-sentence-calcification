a6871af8cbcbf54bdc8d3a5d47bc2b352f00ec7e
Mitsuhiro Komuro <mitsuhiro.komuro.2w@stu.hosei.ac.jp>
2017-09-15 12:35:45 +0900

    Modified data loading mothod
    Create Dataset class.
    The Data set class have two member variable positive data and negative
    data. Now can loading data for dataset using load_data_file method on
    data_loader module.

--- ExecTest.py
+++ ExecTest.py
@@ -1,13 +1,23 @@
 import data_loader
 
+
 def main():
     POS_FILE_PATH = "./data/rt-polaritydata/rt-polarity-utf8.pos"
     NEG_FILE_PATH = "./data/rt-polaritydata/rt-polarity-utf8.neg"
-    in_text , y = data_loader.load_data_file(POS_FILE_PATH, NEG_FILE_PATH)
+    data_set = data_loader.load_data_file(POS_FILE_PATH, NEG_FILE_PATH)
 
-    # print(in_text)
-    for label in y:
-        print(label)
+    print("Positive data")
+    print(data_set.positive_data)
+    print("=============")
+    print("Negative data")
+    print(data_set.negative_data)
+    print("=============")
+    print("Data set information")
+    print("The number of data set : " + str(len(data_set.positive_data) + len(data_set.negative_data)))
+    print("Positive data : " + str(len(data_set.positive_data)))
+    print("Negative data : " + str(len(data_set.negative_data)))
+    print("=============")
+
 
 if __name__ == "__main__":
     main()
--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,13 @@
-import numpy as np
 import re
 
 
-def string_cleaner(string):
+class DataSet:
+    def __init__(self):
+        self.positive_data = []
+        self.negative_data = []
+
+
+def _string_cleaner(string):
     """
     Tokenization/string cleaning for all datasets except for SST.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
@@ -23,49 +28,28 @@ def string_cleaner(string):
     return string.strip().lower()
 
 
-def load_data_and_category(positive_data, negative_data):
+def _load_data(positive_data_path, negative_data_path):
     """
-    Loading a data of categorized sentences from data files.
-    :param positive_data: Positive category data file
-    :param negative_data: Negative category data file
-    :return: Spited sentence and category
+    Loading a data from data files.
+    :param positive_data_path: Positive data file path.
+    :param negative_data_path: Negative data file path.
+    :return: Dataset class object
     """
+    data_set = DataSet()
 
-    # Load the sentences from data files.
-    # Step 1. Load file, Step 2. remove blank word
-    # parameter pos_sentences : List of positive sentence
-    # parameter neg_sentences : List of negative sentence
-    pos_sentences = list(open(positive_data, "r").readlines())
-    pos_sentences = [s.strip() for s in pos_sentences]
-    neg_sentences = list(open(negative_data, "r").readlines())
-    neg_sentences = [s.strip() for s in neg_sentences]
+    # Load files
+    with open(positive_data_path, 'r') as raw_positive_data, \
+            open(negative_data_path, 'r') as raw_negative_data:
+        raw_positive_data.readline()
+        raw_negative_data.readline()
+        raw_positive_sentences = [s.strip() for s in raw_positive_data]
+        raw_negative_sentences = [s.strip() for s in raw_negative_data]
 
-    # print(pos_sentences)
-    # print("===========")
-    # print(neg_sentences)
+    # Formatting data
+    data_set.positive_data = [_string_cleaner(sentence) for sentence in raw_positive_sentences]
+    data_set.negative_data = [_string_cleaner(sentence) for sentence in raw_negative_sentences]
 
-    # Create input sentence
-    # Step 1. Combining 2 list, Step 2. Cleaning sentence format
-    in_sentence = pos_sentences + neg_sentences
-    in_sentence = [string_cleaner(sentence) for sentence in in_sentence]
-
-    # print(in_sentence)
-
-    # Create category labels
-    # If positive sentence, labelled to [0, 1] as positive category.
-    # If negative sentence, labelled to [1, 0] as negative category.
-    pos_category = [[0, 1] for _ in pos_sentences]
-    neg_category = [[1, 0] for _ in neg_sentences]
-
-    # print(pos_category)
-    # print(len(pos_category))
-    # print("===========")
-    # print(neg_category)
-    # print(len(pos_category))
-
-    # Set output format
-    y = np.concatenate([pos_category, neg_category], axis=0)
-    return [in_sentence, y]
+    return data_set
 
 
 def load_data_file(pos_data_file_path, neg_data_file_path):
@@ -73,6 +57,6 @@ def load_data_file(pos_data_file_path, neg_data_file_path):
     Loading datafiles with file path
     :param pos_data_file_path: Positive data files path
     :param neg_data_file_path: Negative data files path
-    :return: Categorized sentence
+    :return: Data set class object
     """
-    return load_data_and_category(pos_data_file_path, neg_data_file_path)
+    return _load_data(pos_data_file_path, neg_data_file_path)
